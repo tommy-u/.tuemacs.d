5a56aeab2a6a24f38e0a7ee1b1db7915eda0b0a8
ttommyy <tunger10@gmail.com>
2016-11-27 13:53:02 -0500

    Add configuration files.

---
+++ my-loadpackages.el
@@ -0,0 +1,11 @@
+; ~/.emacs.d/my-loadpackages.el
+; loading package
+(load "~/.emacs.d/my-packages.el")
+(require 'doremi)
+(require 'color-theme)
+  (setq my-color-themes (list 'color-theme-billw 'color-theme-jsc-dark 
+                              'color-theme-sitaramv-solaris 'color-theme-resolve
+                              'color-theme-classic 'color-theme-jonadabian-slate
+                              'color-theme-kingsajz 'color-theme-shaman
+                              'color-theme-subtle-blue 'color-theme-snowish
+                              'color-theme-sitaramv-nt 'color-theme-wheat))

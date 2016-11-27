b0c32de966f3b2824e09181e2093f2abcbbbf0d8
ttommyy <tunger10@gmail.com>
2016-11-27 15:43:14 -0500

    Changes

--- my-loadpackages.el
+++ my-loadpackages.el
@@ -1,5 +1,6 @@
-; ~/.emacs.d/my-loadpackages.el
-; loading package
+; ~/.tuemacs.d/my-loadpackages.el
+
+;;loading package
 (load "~/.tuemacs.d/my-packages.el")
 (require 'doremi)
 (require 'color-theme)
@@ -7,12 +8,12 @@
 (require 'magit)
 (define-key global-map (kbd "C-c m") 'magit-status)
 
-(require 'yasnippet)
-(yas-global-mode 1)
-(yas-load-directory "~/.emacs.d/snippets")
-(add-hook 'term-mode-hook (lambda()
-			    (setq yas-dont-activate t)))
-
+;; (require 'yasnippet)
+;; (yas-global-mode 1)
+;; (yas-load-directory "~/.tuemacs.d/snippets")
+;; (add-hook 'term-mode-hook (lambda()
+;; 			    (setq yas-dont-activate t)))
+ 
 
 (define-key global-map (kbd "C-S-f") 'windmove-left)
 

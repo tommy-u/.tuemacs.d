62c21c92c4bd1cbe7007def8a6da3b733b9cad81
tommyu <tunger10@gmail.com>
2016-11-27 19:33:19 -0500

    Update my-noexternals.el

--- my-noexternals.el
+++ my-noexternals.el
@@ -2,9 +2,3 @@
 (when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-
-;; Wind-move
-;(global-set-key (kbd "C-c C-j") 'windmove-left)
-;(global-set-key (kbd "C-c C-k") 'windmove-down)
-;(global-set-key (kbd "C-c C-l") 'windmove-up)
-;(global-set-key (kbd "C-c C-;") 'windmove-right)

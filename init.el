c9ab771d4777178594f3fbd5a7b805b2a42e3ed2
ttommyy <tunger10@gmail.com>
2016-12-05 23:33:24 -0500

    Garbage collection, paren mode.

--- init.el
+++ init.el
@@ -91,3 +91,30 @@ Do this after `q` in Debugger buffer."
 
 ;;(setq scroll-step 1) ;; keyboard scroll one line at a time
 
+
+(show-paren-mode 1)
+(setq show-paren-delay 0)
+
+(defadvice show-paren-function
+    (after show-matching-paren-offscreen activate)
+  "If the matching paren is offscreen, show the matching line in the
+        echo area. Has no effect if the character before point is not of
+        the syntax class ')'."
+  (interactive)
+  (let* ((cb (char-before (point)))
+	 (matching-text (and cb
+			     (char-equal (char-syntax cb) ?\) )
+			     (blink-matching-open))))
+    (when matching-text (message matching-text))))
+
+;;If it ain't broke ...
+;;When in minibuffer, don't garbage collect.
+(defun my-minibuffer-setup-hook ()
+  (setq gc-cons-threshold most-positive-fixnum))
+
+;;Until you leave it.
+(defun my-minibuffer-exit-hook ()
+  (setq gc-cons-threshold 800000))
+
+(add-hook 'minibuffer-setup-hook #'my-minibuffer-setup-hook)
+(add-hook 'minibuffer-exit-hook #'my-minibuffer-exit-hook)
